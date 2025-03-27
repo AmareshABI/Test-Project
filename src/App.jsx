8340aa3ae048345ac0e5af39cd5dcd2b14c3dbc4
AmareshABI <ic_amaresh@yahoo.co.in>
2025-03-27 15:17:20 +0530

    ButtonEvent Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './component/Navbar'
-import Contact from './Pages/Contact'
-import Footer from './component/Footer'
-import Card from './component/Card'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import './App.css';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import Card from './component/Card';
 
 function App() {
-  //fetching data from API
-  const [data, setData] = useState([])
-  console.log(data)
+  const [data, setData] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => setData(data))
-  }, [])
- 
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
 
+  const handleNextContact = () => {
+    if (currentIndex < data.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0);
+    }
+  };
 
-  // const user = [
-  //   {Name: "Amaresh",
-  //    Age: "40",
-  //    Gender: "M"
-  //   },
-    
-  //   {Name: "Aakash",
-  //    Age: "24",
-  //    Gender: "M" 
-  //   }
-  // ]
+  const handlePreviousContact = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    } else {
+      setCurrentIndex(data.length - 1);
+    }
+  };
+
+  const handleFirstContact = () => {
+    setCurrentIndex(0);
+  };
+
+  const handleLastContact = () => {
+    setCurrentIndex(data.length - 1);
+  };
 
   return (
     <>
-      <Navbar></Navbar>
-      {/* <Contact></Contact> */}
-      {/* {user.map((item,index) => {
-        return(<Card key={index} Name={item.Name} Age={item.Age} Gender={item.Gender}/>)
-      })
-      }  */}
-      {
-        data.map(user => (
-        <Card key={user.id} user={user} />  
-        ))}
-      <Footer></Footer>
+      <Navbar />
+      <div style={{ textAlign: 'center' }}>
+        <button onClick={handleFirstContact}
+        style={{ backgroundColor: 'lightblue', padding: '10px 15px', margin: '5px', border: 'none', borderRadius: '5px' }}
+        >First Contact</button>
+        <button onClick={handlePreviousContact}
+        style={{ backgroundColor: 'lightblue', padding: '10px 15px', margin: '5px', border: 'none', borderRadius: '5px' }}
+        >Previous Contact</button>
+        <button onClick={handleNextContact}
+        style={{ backgroundColor: 'lightblue', padding: '10px 15px', margin: '5px', border: 'none', borderRadius: '5px' }}
+        >Next Contact</button>
+        <button onClick={handleLastContact}
+        style={{ backgroundColor: 'lightblue', padding: '10px 15px', margin: '5px', border: 'none', borderRadius: '5px' }}
+        >Last Contact</button>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {data.length > 0 && (
+          <div style={{ width: '500px' }}>
+            <Card key={data[currentIndex].id} user={data[currentIndex]} />
+          </div>
+        )}
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
